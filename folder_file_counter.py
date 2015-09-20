2697218977fdd5963b492285f1625b113d081e7a
shawndaniel <shawn@founderimpact.com>
2015-09-20 18:09:23 +0700

    readability

--- folder_file_counter.py
+++ folder_file_counter.py
@@ -15,15 +15,14 @@ import operator
 
 
 path = 'C:\\test'
-# list all the directories from "path" variable and create a dictionary with each of the folders as the keys
-# and 0 as their default values
-folders = {key: 0 for key in os.listdir(path) if os.path.isdir(os.path.join(path, key))}
+folders = {key: 0 for key in os.listdir(path) if os.path.isdir(os.path.join(path, key))}   # list all the directories
+# from "path" variable and create a dictionary with each of the folders as the keys and 0 as their default values
 
 def count_files():
     for folder in folders.keys():
         file_count = len(os.listdir(os.path.join(path, user)))     # for each folder assign the length of each folder
         folders[folder] = file_count    # assign file_count as the (value) to the corresponding folder (key)
-    sorted_count = sorted(folders.iteritems(), key=operator.itemgetter(1), reverse=True) # iterate through dictionary
+    sorted_count = sorted(folders.iteritems(), key=operator.itemgetter(1), reverse=True) # iterate through dictionary,
     #  assign items as tuples inside new list "sorted_count", then sorts them by their values in descending order
     for x, y in sorted_count:
         if y > 0:
