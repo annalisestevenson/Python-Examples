8b3c46b1a33db54342045ec61e5ebb4717b10973
shawndaniel <shawn@founderimpact.com>
2015-09-22 23:35:51 +0700

    added calculation of the age of each folder
    messing around with the time module

--- folder_file_counter.py
+++ folder_file_counter.py
@@ -10,9 +10,10 @@ for python beginners and can be generally useful when more capabilities added.
 
 import os
 from sys import argv
-
+import time
 
 script, path = argv
+# path = 'C:\\test'
 # list all directories within "path" variable and create a dict with each of them as the keys and 0 as their value
 folders = {key: 0 for key in os.listdir(path) if os.path.isdir(os.path.join(path, key))}   
 
@@ -20,13 +21,19 @@ folders = {key: 0 for key in os.listdir(path) if os.path.isdir(os.path.join(path
 def count_files():
     for folder in folders.keys():   # for each folder count the length of it
         file_count = len(os.listdir(os.path.join(path, folder)))
-        folders[folder] = file_count
+        current_date = time.localtime()
+        folder_date = time.localtime(os.stat(os.path.join(path, folder)).st_ctime)
+        month = current_date[1] - folder_date[1]
+        day = current_date[2] - folder_date[2]
+        year = current_date[0] - folder_date[0]
+        duration_formatted = "%d/%d/%d" % (month, day, year)
+        folders[folder] = file_count, duration_formatted
 # return copy of items as tuples inside new list, then sort them by their values in descending order
     sorted_count = sorted(folders.items(), key=(lambda x: x[1]), reverse=True)
 # if file count > 0, print each folder with it's count number on each line
     for x, y in sorted_count:
-        if y > 0:
-            print x, y
+        if y[0] > 0:
+            print "%s ---- %s ---- %s" % (x, y[0], y[1])
         else:
             continue
 
