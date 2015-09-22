04c62af35a10ce786e1ad04c47fb3e6a43d489ae
shawndaniel <shawn@founderimpact.com>
2015-09-22 16:33:27 +0700

    tried variation that doesn't require importing operator module

--- folder_file_counter.py
+++ folder_file_counter.py
@@ -1,7 +1,3 @@
-import os
-import operator
-from sys import argv
-
 __author__ = 'Shawn Daniel'
 """
     This simple script counts the number of files within all the directories
@@ -11,6 +7,10 @@ per folder by descending order. More capabilities are planning to be added
 Initially started writing this for some server software although it can be generally
 useful when more capabilities added. Also a good example for beginner programmers.
 """
+
+import os
+from sys import argv
+
 
 script, path = argv
 # list all directories within "path" variable and create a dict with each of them as the keys and 0 as their value
@@ -22,7 +22,7 @@ def count_files():
         file_count = len(os.listdir(os.path.join(path, folder)))
         folders[folder] = file_count
 # iterate through items as tuples inside new list, then sort them by their values in descending order
-    sorted_count = sorted(folders.iteritems(), key=operator.itemgetter(1), reverse=True)
+    sorted_count = sorted(folders.items(), key=(lambda x: x[1]), reverse=True)
 # if file count > 0, print each folder with it's count number on each line
     for x, y in sorted_count:
         if y > 0:
